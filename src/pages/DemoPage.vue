eff9a78ccfbeec1ab42fbb741d818a4cde120b28
Ellomend <mrsotona@gmail.com>
2022-10-29 07:39:38 +0300

    card2

--- src/pages/DemoPage.vue
+++ src/pages/DemoPage.vue
@@ -13,6 +13,16 @@
         {{ todos }}
       </q-card-section>
     </q-card>
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ todos }}
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
